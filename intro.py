dd0d22b2ea25dc5f98a5bb9c4d273952db846c6b
sathish07-dotcom <ejjagirisathish6@gmail.com>
2025-04-06 17:59:52 +0530

    first commit

---
+++ intro.py
@@ -0,0 +1,106 @@
+#FIRST PROGRAM IN PYTHON
+
+print("ejjagiri Sathish")
+
+print("Good Morning")
+
+print( 10 + 30)
+
+
+age= 20
+print("Your's age is:",age)
+
+
+#COMMENTS IN PYTHON
+
+print('1.SINGLE LINE')#single line
+print('2.MULTILINE')
+'''  
+
+print("sagar")
+print("rahul")
+ '''
+
+
+#VARIBLES IN PYTHON
+
+a=20
+print("a=",a)
+
+b= 30
+print("b=",b)
+
+a=100
+print("a=",a)
+
+
+
+# DATA TYPES in PYTHON
+
+a=20   # integres
+print(a)
+
+a= -999
+print(a)
+
+b=99.9 #floating
+print(b)
+
+c ="sagar" # srtings
+print(c)
+
+is_follwing = True # booleans
+print(is_follwing)
+
+#Lists  Similar like Arrays
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+#Dictionaries  Similar like Objects in javascript
+student ={
+    "name":"sagar",
+    "age": 25
+}
+print(student)
+
+'''
+# Operators in Python SAME AS JAVASCRIPT
+
+#Arithmetic Operators: Used to perform basic mathematical operations.
+a=20
+b=10
+
+print("sum",a+b)
+print("diff",a-b)
+print("multpy",a*b)
+print("div",a/b)
+print(a%b)
+
+a=2
+b=4
+print("squre",a**b)
+
+
+
+#Comparison Operators: Used to compare values and return a boolean (`True` or `False`).
+
+a=30
+b=50
+
+print(a !=b)
+print(a<b)
+print(a>b)
+
+#Logical Operators: Used to combine conditional statements.
+
+a=20
+b=50
+print(a>0 and b<100)
+
+print(a >0 or b>100)
+
+print(not b<100)
+print(not b>100)
+
+'''
---
+++ more.py
@@ -0,0 +1,63 @@
+name = 'My Name is sagar'
+print(name)
+print(type(name))
+
+age =23
+print(age)
+print(type(age))
+
+num1 = 50
+num2 =49.9
+result = num1 + num2
+print(result)
+print(type(result))
+
+str = "is_python_fun as True"
+print(str)
+print(type(str))
+
+bio = ''' Ejjagiri sathish 
+mca student from kakatiya university
+with 75%
+he got job in a college placement
+'''
+
+print(bio)
+print(type(bio))
+
+float = 6.9
+print(float)
+num = int(float)
+print(num)
+
+str = "100"
+print(str)
+print(type(str))
+num_int = int(str)
+print(num_int)
+print(type(num_int))
+
+
+a= 20
+b=30
+print("a=",a,"b=",b,"Before Swapping")
+
+b=a
+
+print(a,b,"After Swapping")
+
+
+fav_items =["mobile","TV","bookes","laptop","dresses","shoes"]
+print(fav_items)
+print(fav_items[2])
+print(fav_items[0])
+
+
+info ={
+    "name": "laddu",
+    "grade": "A+",
+    "age": 27,
+    "clg": "Au",
+}
+print(info)
+print(age)
